42e037586d79d437f61faaa4096deb60624f4a6c
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-19 20:30:45 -0300

    feat: Modificando o layout para (home) e adicionando section-title

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import SectionTitle from "@/components/ui/section-title";
+
+const Home = () => {
+  return (
+    <div className="mx-auto flex flex-col gap-8 py-8 lg:container lg:gap-10">
+      <div className="flex flex-col gap-3 lg:gap-5">
+        <SectionTitle className="pl-5">Certificados</SectionTitle>
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
