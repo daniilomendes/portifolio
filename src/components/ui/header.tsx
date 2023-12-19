61de5e43439d21e9cce690a2fd86b0db4e0ccbb7
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-19 20:18:23 -0300

    feat: Adicionando opções no menu lateral

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,69 @@
-import { MenuIcon } from "lucide-react";
+import { FolderKanban, HomeIcon, MenuIcon, ShieldCheck } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
 import Link from "next/link";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left" className="w-[21.875rem]">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-4 flex flex-col gap-2">
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ShieldCheck size={16} />
+                  Certificados
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <FolderKanban size={16} />
+                  Projetos
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <Link href="/">
         <h1 className="text-lg font-semibold">
