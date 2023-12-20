71baac573f879a92cf0daa95186801223dab4235
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-20 19:13:40 -0300

    feat: Garantindo que os certificados estão aparecendo na certificates page

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -1,5 +1,27 @@
+import CertificatesItem from "@/components/ui/certificates-item";
+
 const CertificatesPage = () => {
-    return ( <div>Certificados</div> );
-}
- 
+  const imagesCertificates: Array<string> = [
+    "/Banco-de-Dados-SQL-NodeJS.png",
+    "/imgGit.png",
+    "/Banco-de-Dados-SQL-NodeJS.png",
+    "/imgGit.png",
+  ];
+  const namesCertificates: Array<string> = [""];
+  const categoryCertificates: Array<string> = [""];
+  const linksCertificates: Array<string> = [""];
+
+  return (
+    <div>
+      <div className="mx-auto flex flex-col gap-8 p-5 lg:container lg:gap-10 lg:py-10">
+        <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
+          {imagesCertificates.map((cert) => (
+            <CertificatesItem certificate={cert} className="" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CertificatesPage;
