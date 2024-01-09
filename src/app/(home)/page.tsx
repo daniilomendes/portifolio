284f75e51a2c0b2a4756d7a9df348fb16b231a4d
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-08 22:23:12 -0300

    feat: Garantindo que o CertificatesCarrousel está funcinal na página inicial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import CertificatesList from "@/components/ui/certificates-list";
+import CardSlider from "@/components/ui/CertificatesCarrousel";
 import IntroductionArea from "@/components/ui/introduction-area";
 import ProjectList from "@/components/ui/project-list";
 import SectionTitle from "@/components/ui/section-title";
@@ -6,7 +6,7 @@ import SectionTitle from "@/components/ui/section-title";
 const Home = () => {
   return (
     <div className="mx-auto flex flex-col gap-8 py-8 lg:container lg:gap-10">
-      {/* Texto de introdução */}
+
       <div className="flex flex-col gap-3 lg:gap-5">
         <SectionTitle className="pl-5">Sobre mim</SectionTitle>
         <IntroductionArea />
@@ -14,11 +14,11 @@ const Home = () => {
 
       <div className="flex flex-col gap-3 lg:gap-5">
         <SectionTitle className="pl-5">Certificados</SectionTitle>
-        <CertificatesList />
+        <CardSlider />
       </div>
 
       <div>
-        <SectionTitle className="pl-5 mb-8 lg:mb-1">Projetos</SectionTitle>
+        <SectionTitle className="mb-8 pl-5 lg:mb-1">Projetos</SectionTitle>
         <ProjectList />
       </div>
     </div>
