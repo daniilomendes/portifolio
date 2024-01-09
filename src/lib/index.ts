8fc9af2f7b3f952ebc5443f7f2fccecd94f738c8
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-08 22:21:02 -0300

    feat: Criando o components Slider

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+import Slider from "@/components/ui/Slider";
+import { SwiperProps as SliderProps, SwiperSlide } from "swiper/react";
+
+export { SwiperSlide as Slide, Slider };
+export type { SliderProps };
