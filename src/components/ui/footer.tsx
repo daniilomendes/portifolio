0db1496c858ce83da4df6b047123a64368614eac
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-29 11:01:41 -0300

    chore: adicionando novos projetos

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
       <div className="mx-auto lg:container">
-        © 2023 Copyright <span className="font-semibold">Meu Portfólio</span>
+        © 2023 Copyright <span className="font-semibold">Danilo Mendes</span>
       </div>
     </div>
   );
--- src/components/ui/project-list.tsx
+++ src/components/ui/project-list.tsx
@@ -10,55 +10,62 @@ interface Project {
 const ProjectList = () => {
   let project1 = {
     id: 1,
+    image: "/projects-images/social-media.png",
+    name: "Social Media",
+    link: "https://social-media-plum-three.vercel.app/sign-in"
+  }
+
+  let project2 = {
+    id: 2,
     image: "/projects-images/ecd-store.png",
     name: "E-commerce ECD",
     link: "https://e-commerce-daniilomendes-projects.vercel.app/",
   };
 
-  let project2 = {
-    id: 2,
-    image: "/projects-images/menu-online.png",
-    name: "Menu Online",
-    link: "https://site-de-vendas-lanches-daniilomendes-projects.vercel.app/",
-  };
-
   let project3 = {
     id: 3,
-    image: "/projects-images/mini-e-commerce.png",
-    name: "Mini E-commerce",
-    link: "https://black-nextjs-daniilomendes-projects.vercel.app/",
+    image: "/projects-images/menu-online.png",
+    name: "Menu Online",
+    link: "https://incandescent-moxie-19bd07.netlify.app",
   };
 
   let project4 = {
     id: 4,
+    image: "/projects-images/youtube-clone.png",
+    name: "Youtube clone",
+    link: "https://timely-sopapillas-93d31c.netlify.app/",
+  };
+
+  let project5 = {
+    id: 5,
     image: "/projects-images/gestor-de-estoque.png",
     name: "Gestor de Estoque",
     link: "https://stupendous-alpaca-ebe272.netlify.app/",
   };
 
   
-  let project5 = {
-    id: 5,
+  let project6 = {
+    id: 6,
     image: "/projects-images/calculadora.png",
     name: "Calculadora",
     link: "https://willowy-scone-0f20f8.netlify.app/",
   };
 
-  let project6 = {
-    id: 6,
+  let project7 = {
+    id: 7,
     image: "/projects-images/jogo_da_velha.png",
     name: "Jogo da Velha",
     link: "https://splendorous-sprite-c66f9a.netlify.app/",
   };
 
-  let project7 = {
-    id: 7,
+  let project8 = {
+    id: 8,
     image: "/projects-images/to_do.png",
     name: "Jogo da Velha",
     link: "https://magnificent-kelpie-a2ca22.netlify.app/",
   };
 
-  const projects: Project[] = [project1, project2, project3, project4, project5, project6, project7];
+  const projects: Project[] = [project1, project2, project3, project4, project5, project6, project7, project8];
 
   return (
     <div className="mx-auto flex flex-col gap-8 pl-5 pr-5 lg:container lg:gap-10 lg:py-10">
