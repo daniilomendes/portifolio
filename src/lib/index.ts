a1d0d636faea5ff5495cb69b612327f6fadb7d03
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-13 18:04:46 -0300

    fix: removendo slider

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import Slider from "@/components/ui/Slider";
-import { SwiperProps as SliderProps, SwiperSlide } from "swiper/react";
-
-export { SwiperSlide as Slide, Slider };
-export type { SliderProps };
