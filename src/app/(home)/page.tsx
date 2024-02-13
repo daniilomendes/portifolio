61b6492d03fb28fad42d677a466108103f8f3489
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-13 17:22:19 -0300

    raw: remoção de comentarios

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
-import CardSlider from "@/components/ui/CertificatesCarrousel";
 import IntroductionArea from "@/components/ui/introduction-area";
-import ProjectList from "@/components/ui/project-list";
+import ProjectList from "@/app/projects/components/project-list";
 import SectionTitle from "@/components/ui/section-title";
 
 const Home = () => {
@@ -10,11 +9,6 @@ const Home = () => {
         <SectionTitle className="pl-5">Sobre mim</SectionTitle>
         <IntroductionArea />
       </div>
-
-      {/* <div className="flex flex-col gap-3 md:hidden lg:gap-5">
-        <SectionTitle className="pl-5">Certificados</SectionTitle>
-        <CardSlider />
-      </div> */}
 
       <div>
         <SectionTitle className="mb-1 md:mb-8 pl-5 pb-5">Projetos</SectionTitle>
