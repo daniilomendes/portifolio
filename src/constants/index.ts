3e9e3d0db7ade4d64c1a86a4d6679e1f1b545334
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-13 16:18:20 -0300

    feat: modificação na origem dos links de imagens e add novos projetos

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,217 +2,328 @@
 
 interface Project {
   id: number;
-  image: string;
+  imageUrl: string;
   name: string;
   link: string;
 }
 
-let project1 = {
+let socialMedia = {
   id: 1,
-  image: "/projects-images/social-media.png",
+  imageUrl: "https://utfs.io/f/b34721b0-cdb1-4722-86f7-f0b3017effd1-lbpkzo.png",
   name: "Social Media",
   link: "https://social-media-plum-three.vercel.app/",
 };
 
-let project2 = {
+let eCommerce = {
   id: 2,
-  image: "/projects-images/ecd-store.png",
+  imageUrl: "https://utfs.io/f/22db3ef3-016c-4b16-9cfb-5327bd747d05-le3xoj.png",
   name: "E-commerce ECD",
   link: "https://e-commerce-daniilomendes-projects.vercel.app/",
 };
 
-let project3 = {
+let menuOnline = {
   id: 3,
-  image: "/projects-images/menu-online.png",
+  imageUrl: "https://utfs.io/f/0821f123-f8a5-4d50-a57a-d49015a99a8e-1zr6n.png",
   name: "Menu Online",
   link: "https://incandescent-moxie-19bd07.netlify.app",
 };
 
-let project4 = {
+let youtubeClone = {
   id: 4,
-  image: "/projects-images/youtube-clone.png",
+  imageUrl: "https://utfs.io/f/c05eb9ed-81c3-4f44-890d-e28c4ca907e1-x2mz4d.png",
   name: "Youtube clone",
   link: "https://timely-sopapillas-93d31c.netlify.app/",
 };
 
-let project5 = {
+let gestorDeEstoque = {
   id: 5,
-  image: "/projects-images/gestor-de-estoque.png",
+  imageUrl: "https://utfs.io/f/7d0e6bab-68ae-4962-950b-3a229ac641bf-ge4cj7.png",
   name: "Gestor de Estoque",
   link: "https://stupendous-alpaca-ebe272.netlify.app/",
 };
 
-let project6 = {
+let calculadora = {
   id: 6,
-  image: "/projects-images/calculadora.png",
+  imageUrl: "https://utfs.io/f/7106ca2e-b682-4578-876b-173d44c105d4-fgrl8x.png",
   name: "Calculadora",
   link: "https://willowy-scone-0f20f8.netlify.app/",
 };
 
-let project7 = {
+let jogoDaVelha = {
   id: 7,
-  image: "/projects-images/jogo_da_velha.png",
+  imageUrl: "https://utfs.io/f/233176a2-e28a-4fdd-8942-f032190cec5c-z8c8za.png",
   name: "Jogo da Velha",
   link: "https://splendorous-sprite-c66f9a.netlify.app/",
 };
 
-let project8 = {
+let toDo = {
   id: 8,
-  image: "/projects-images/to_do.png",
-  name: "Jogo da Velha",
+  imageUrl: "https://utfs.io/f/5b590e6e-1126-490c-bf81-283d27c8391b-1ts07x.png",
+  name: "Lista To Do",
   link: "https://magnificent-kelpie-a2ca22.netlify.app/",
 };
 
+let vitrineDeCarros = {
+  id: 9,
+  imageUrl: "https://utfs.io/f/d1b7f0fc-0ee7-4d5b-9931-8f17df75b7b5-soita8.png",
+  name: "Vitrine de Carros",
+  link: "https://coruscating-centaur-cb3cf8.netlify.app/",
+};
+
+let resumidorDeArtigos = {
+  id: 10,
+  imageUrl: "https://utfs.io/f/1242d4bb-bba2-4a2c-b4fc-d6e5ca3b2021-ticoi6.png",
+  name: "Resumidor de Artigos",
+  link: "https://comfy-mousse-2932bb.netlify.app/",
+};
+
+let quiz = {
+  id: 11,
+  imageUrl: "https://utfs.io/f/cea7f678-57e0-4187-bd7a-89d34d92c798-22mvp.png",
+  name: "Quiz",
+  link: "https://animated-conkies-433c76.netlify.app/",
+};
+
+let pokedex = {
+  id: 12,
+  imageUrl: "https://utfs.io/f/ccd68abf-e5d7-42ac-ad02-b6404c2b5afa-6lkuua.png",
+  name: "Pokedex",
+  link: "https://fascinating-blini-513ce6.netlify.app/",
+};
+
+let localizadorDeGitHub = {
+  id: 13,
+  imageUrl: "https://utfs.io/f/9f4c6f5d-d5c0-4e16-b4a7-bb6f69e16477-pj8qxc.png",
+  name: "Localizador de GitHubs",
+  link: "https://sparkling-bienenstitch-901450.netlify.app/",
+};
+
+let geradorDeSenhas = {
+  id: 14,
+  imageUrl: "https://utfs.io/f/9001f569-cf34-4466-9898-c95d2d87b7df-8nojov.png",
+  name: "Tela de Login com Gerador de Senhas",
+  link: "https://fancy-buttercream-402fcb.netlify.app/",
+};
+
+let geradorDeQrCode = {
+  id: 15,
+  imageUrl: "https://utfs.io/f/a5bfb53f-08c0-4564-a6f6-bb8f468c18b5-8gpr8a.png",
+  name: "Gerador de Qr-Code",
+  link: "https://bright-manatee-b711d0.netlify.app/",
+};
+
+let devNotes = {
+  id: 16,
+  imageUrl: "https://utfs.io/f/da40a5e4-20c1-4707-8230-82f383f8ce5e-rtgfsn.png",
+  name: "Notas Post-it",
+  link: "https://glistening-phoenix-5c72e7.netlify.app/",
+};
+
+let dashboard = {
+  id: 17,
+  imageUrl: "https://utfs.io/f/af44742a-8651-4e23-9f44-2b1b8593c16b-l8o1dy.png",
+  name: "Dashboard",
+  link: "https://shimmering-starship-db869e.netlify.app/",
+};
+
+let calculadoraSimples = {
+  id: 18,
+  imageUrl: "https://utfs.io/f/40429b47-5fd5-4726-a944-8896cae4fecc-k28zqb.png",
+  name: "Calculadora Simples",
+  link: "https://lighthearted-kataifi-7500ae.netlify.app/",
+};
+
+let calculadoraImc = {
+  id: 19,
+  imageUrl: "https://utfs.io/f/51e0ae27-336f-4c85-b3c4-db0ede8f48b2-yo5dgf.png",
+  name: "Calculadora de IMC",
+  link: "https://fluffy-nasturtium-e06e48.netlify.app/",
+};
+
+let geradorDeBoxShadow = {
+  id: 20,
+  imageUrl: "https://utfs.io/f/c31034ed-3bcf-480b-b699-dd04af96fd22-tfapwe.png",
+  name: "Gerador de Box Shadow",
+  link: "https://chic-klepon-bea224.netlify.app/",
+};
+
+let blog = {
+  id: 21,
+  imageUrl: "https://utfs.io/f/785a0f27-3fb6-4e1e-a9b1-eb1493efe7a1-1svj6.png",
+  name: "Blog Simples",
+  link: "https://clever-faloodeh-4fea49.netlify.app/",
+};
+
+let barbearia = {
+  id: 22,
+  imageUrl: "https://utfs.io/f/ace5f5d3-2ae0-466b-9f60-9187c8f7fff7-n3a63o.png",
+  name: "Barbearia",
+  link: "https://barbearia-daniilomendes-projects.vercel.app/",
+};
+
 export const projects: Project[] = [
-  project1,
-  project2,
-  project3,
-  project4,
-  project5,
-  project6,
-  project7,
-  project8,
+  socialMedia,
+  eCommerce,
+  resumidorDeArtigos,
+  dashboard,
+  menuOnline,
+  youtubeClone,
+  barbearia,
+  vitrineDeCarros,
+  quiz,
+  devNotes,
+  blog,
+  gestorDeEstoque,
+  calculadora,
+  jogoDaVelha,
+  toDo,
+  localizadorDeGitHub,
+  pokedex,
+  calculadoraImc,
+  calculadoraSimples,
+  geradorDeBoxShadow,
+  geradorDeQrCode,
+  geradorDeSenhas,
 ];
 
 // Certificados
 
 interface Certificate {
   id: number;
-  image: string;
+  imageUrl: string;
   name: string;
   link: string;
 }
 
 let certificate1 = {
   id: 1,
-  image:
-    "/certificates-images/Programador-Full-Stack-JavaScript-Profissional.png",
+  imageUrl: "https://utfs.io/f/03fab1eb-83c8-41d0-a58a-3691eb6f0c1c-ydzrab.png",
   name: "Programador Full Stack JavaScript Profissional",
   link: "https://drive.google.com/file/d/1r6VVoSt35_hZWfmawnjqjGg5y_LdvnVG/view?usp=sharing",
 };
 let certificate2 = {
   id: 2,
-  image: "/certificates-images/TypeScript.png",
+  imageUrl: "https://utfs.io/f/91fccbaf-d87e-4389-996e-2e968da5af7b-96gkat.png",
   name: "TypeScript",
   link: "https://drive.google.com/file/d/16rkUEnI4BrT7jk7KZUMgIdE0bvDAXCH3/view?usp=sharing",
 };
 let certificate3 = {
   id: 3,
-  image: "/certificates-images/Formação-Front-end.jpg",
+  imageUrl: "https://utfs.io/f/9dec585d-3643-4443-a71f-25454f15714a-3tl340.jpg",
   name: "Formação Front-End",
   link: "https://drive.google.com/file/d/114wi9y3y3SEKBVeqXTUyUK-z4fPVklRM/view?usp=sharing",
 };
 let certificate4 = {
   id: 4,
-  image: "/certificates-images/Dominando-NodeJs.png",
+  imageUrl: "https://utfs.io/f/fd4b3280-b523-405d-8ff9-af454b49d16c-q19vix.png",
   name: "Dominando o NodeJs",
   link: "https://drive.google.com/file/d/18-bvI89r8Dc5Vk5CR-JnMgRLWAWUZP8-/view?usp=sharing",
 };
 let certificate5 = {
   id: 5,
-  image: "/certificates-images/NextJS.png",
+  imageUrl: "https://utfs.io/f/fe97fa7d-adaf-46f8-9dd2-a97a6ea58bb8-whv90k.png",
   name: "NextJS",
   link: "https://drive.google.com/file/d/134CI-NtTWm38jbPToZkFX04152c31AeZ/view?usp=sharing",
 };
 let certificate6 = {
   id: 6,
-  image: "/certificates-images/Fundamentos-React.png",
+  imageUrl: "https://utfs.io/f/06b2ef3c-bc99-4249-8032-9ec4aef8f5a5-oig18w.png",
   name: "Fundamentos do React",
   link: "https://drive.google.com/file/d/16wWCqRVhJcEUNUpBqGJ1EeaR2FCpgUw0/view?usp=sharing",
 };
 let certificate7 = {
   id: 7,
-  image: "/certificates-images/Gestão-Ágil-Scrum.jpg",
+  imageUrl: "https://utfs.io/f/e7e1923a-5387-4370-bc99-c93100785e05-aa79h4.jpg",
   name: "Gestão Ágil com Scrum",
   link: "https://drive.google.com/file/d/19_wxJFapZDJ10Urn_PvfcP1n7pcnBYvD/view?usp=sharing",
 };
 let certificate8 = {
   id: 8,
-  image: "/certificates-images/Banco-de-Dados-SQL-NodeJS.png",
+  imageUrl: "https://utfs.io/f/b3c5c67f-3475-433a-a64e-0347b7c0baf2-he26t6.png",
   name: "Banco de Dados SQL NodeJS",
   link: "https://drive.google.com/file/d/1vCFbwz0vzFdxhlZSnDpOgeLgyS7O5ZXz/view?usp=sharing",
 };
 let certificate9 = {
   id: 9,
-  image: "/certificates-images/JavaScript.png",
+  imageUrl: "https://utfs.io/f/bfbb8a7f-952f-450b-b745-3edb44d3d6cb-kxxsx9.png",
   name: "JavaScript",
   link: "https://drive.google.com/file/d/1KO4_DvmG3S4VMfmGaO9ldXHQ6WwA93tm/view?usp=sharing",
 };
 let certificate10 = {
   id: 10,
-  image: "/certificates-images/MongoDB.png",
+  imageUrl: "https://utfs.io/f/6cd8803a-b4fa-4809-afc8-4475a43971c0-n1zjqm.png",
   name: "MongoDB",
   link: "https://drive.google.com/file/d/1TLW5B5NYWunmITzrZ6IdZBsyOSgow71o/view?usp=sharing",
 };
 let certificate11 = {
   id: 11,
-  image: "/certificates-images/C-Completo-POO.png",
+  imageUrl: "https://utfs.io/f/c323ce98-8ba7-45e6-adeb-80bd22dfe840-j1df77.png",
   name: "C# Completo e Programação Orientada a Objetos",
   link: "https://drive.google.com/file/d/1CQViZxzXNhEnmETj4mA3O2cs_TwXSZT7/view?usp=sharing",
 };
 let certificate12 = {
   id: 12,
-  image: "/certificates-images/Programação-Python-básico-avançado.jpg",
+  imageUrl: "https://utfs.io/f/129ce383-6c16-4f6d-ac4e-b1de7644ce01-h4bw6x.jpg",
   name: "Programação Python do Básico ao Avançado",
   link: "https://drive.google.com/file/d/1ioQ5hEr_S6yuMhXYApsNdeWoV7xsKIGv/view?usp=sharing",
 };
 let certificate13 = {
   id: 13,
-  image: "/certificates-images/Formação-Cientista-Dados.jpg",
+  imageUrl: "https://utfs.io/f/26385a98-8c7c-4279-aa46-ddc26704288c-89hwyl.jpg",
   name: "Formação Cientista de Dados com Python e R",
   link: "https://drive.google.com/file/d/1zfrUVqSSKaWXD7srmObd5uoQuPrZ3f_5/view?usp=sharing",
 };
 let certificate14 = {
   id: 14,
-  image: "/certificates-images/Planejamento-e-Gestão-Projetos.jpg",
+  imageUrl: "https://utfs.io/f/b43ad6a1-bf3c-47c5-b826-23f274b95987-bc074r.jpg",
   name: "Planejamento e Gestão Projetos",
   link: "https://drive.google.com/file/d/1miaLpUoZ_NgmHksuQtxLkMsL4EI4hk3L/view?usp=sharing",
 };
 let certificate15 = {
   id: 15,
-  image: "/certificates-images/Sass.png",
+  imageUrl: "https://utfs.io/f/d6ef89da-083d-4243-8781-2e9b55583b5d-1j2oe.png",
   name: "Sass",
   link: "https://drive.google.com/file/d/1vdBGEil8pHjTRFl-_l6oWWQzcLirCJMu/view?usp=sharing",
 };
 let certificate16 = {
   id: 16,
-  image: "/certificates-images/HTML5.png",
+  imageUrl: "https://utfs.io/f/ee1224f5-385f-4cb8-bbb6-6339f6e3661e-154gkq.png",
   name: "HTML5",
   link: "https://drive.google.com/file/d/1x-7avivdn9oRax08-VJFjd-1-rD2Gawm/view?usp=sharing",
 };
 let certificate17 = {
   id: 17,
-  image: "/certificates-images/Git-GitHub.png",
+  imageUrl: "https://utfs.io/f/1b8b1643-863a-400d-a0ff-29e8584841bf-ujmjby.png",
   name: "Git e GitHub",
   link: "https://drive.google.com/file/d/1hwZe4kGN0bqmaakHgLJDq9CRO_DmdF7A/view?usp=sharing",
 };
 let certificate18 = {
   id: 18,
-  image: "/certificates-images/Bootstrap.png",
+  imageUrl: "https://utfs.io/f/373e72b5-a0a8-428e-aca1-be19c2d84127-5xetq.png",
   name: "Bootstrap",
   link: "https://drive.google.com/file/d/17iGmi3w8nu2haEXt1Th3ndvfrTdB5x7z/view?usp=sharing",
 };
 let certificate19 = {
   id: 19,
-  image: "/certificates-images/CSS-Moderno.png",
+  imageUrl: "https://utfs.io/f/760f2c63-a59e-4cae-91c3-d1116ffaaceb-cfdqna.png",
   name: "CSS Moderno",
   link: "https://drive.google.com/file/d/16JbNMZ_B3iyaIUSKobzyJFPVQqwWRkMC/view?usp=sharing",
 };
 let certificate20 = {
   id: 20,
-  image: "/certificates-images/CSS3.png",
+  imageUrl: "https://utfs.io/f/7e99edfe-dc4d-44c4-95ad-e031cc4bf287-18jow.png",
   name: "CSS3",
   link: "https://drive.google.com/file/d/1s5V45Li2RnK7-7aL9sxs19P2C_oUITUd/view?usp=sharing",
 };
 let certificate21 = {
   id: 21,
-  image: "/certificates-images/Bulma.png",
+  imageUrl: "https://utfs.io/f/be6e3d6b-099a-481c-9836-71a262751c8f-12ffe5.png",
   name: "Bulma",
   link: "https://drive.google.com/file/d/1uw9X2soUCd4-mMUkYKZ4VG7bBzUVXCOO/view?usp=sharing",
 };
 let certificate22 = {
   id: 22,
-  image: "/certificates-images/KanBan-Guia-Completo.jpg",
+  imageUrl: "https://utfs.io/f/df83aff5-1dba-444c-9ab6-8f191bde3b30-4srxxw.jpg",
   name: "KanBan: O Guia Completo",
   link: "https://drive.google.com/file/d/1gVpCwcnNf9xn4NMlnljFxuBfSvc7xknk/view?usp=sharing",
 };
